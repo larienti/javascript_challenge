6780fc7d322c32d0cb5455d96a0e893555b5c940
larienti <lars.arienti@gmail.com>
2020-05-20 14:41:51 -0400

    js challenge part 1

---
+++ ufo_level_1/static/js/appufo1.js
@@ -0,0 +1,42 @@
+// import data
+var tableData = data;
+var button = d3.select("#filter-button");
+var selection = de.select("form");
+
+//runEnter when form submitted65
+button.on("click", formSubmission);
+form.on("submit", formSubmission);
+
+// select html elements tbody, table
+
+var tbody= d3.select("tbody");
+var table= d3.select("table");
+
+//format data.js
+tableData.forEach((i) => {
+    var currentRow = tbody.append('tr');
+    Object.entries(i).forEach(([k,v]) => 
+    {
+        var currentEntry = currentRow.append('td');
+        currentEntry.text(v);
+    });
+});
+
+function formSubmission() {
+    d3.event.preventDefault();
+    //select and output data
+    var inputDate = d3.select('#datetime').property('value');
+    var filteredData = tableData.filter(r => r.datetime === inputDate);
+    //console.log(filteredData);
+    // empty tbody tag opening, iterate data
+    tbody.html("");
+    filteredData.forEach((i) => 
+    {
+        var rowX = tbody.append('tr');
+        Object.entries(i).forEach(([k,v]) => 
+        {
+            var cell = rowX.append('td');
+            cell.text(v);
+        });
+    });
+}
